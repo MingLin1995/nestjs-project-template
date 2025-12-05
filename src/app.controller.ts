b0f65472241e5b6af78b9a2d32271483be2ded0b
Ming <ben014335@gmail.com>
2025-12-05 13:18:59 +0800

    feat：連線測試 API 回傳值新增當前版本

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { Public } from './common/decorators/public.decorator';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @ApiTags('API Check')
 @Controller()
 export class AppController {
+  private readonly appVersion: string;
+
+  constructor() {
+    const packageJsonPath = path.resolve(__dirname, '../../package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+    this.appVersion = packageJson.version;
+  }
+
   @Public()
   @Get()
   @ApiOperation({ summary: 'API 連線測試' })
@@ -21,6 +31,7 @@ export class AppController {
         minute: '2-digit',
         hour12: false,
       }),
+      version: this.appVersion,
     };
   }
 }
